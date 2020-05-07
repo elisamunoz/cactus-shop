856ab200aa79315dd6412632ec161da70f93c99f
Elisa Munoz <elisamunozes@gmail.com>
2020-05-07 23:48:52 +0100

    added secret key os environ

--- catcus/settings.py
+++ catcus/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%d9z%)u)@@%tsxd!6k*9v8(gfvke+a2(2zwm-z8qivec#ci*b!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "%d9z%)u)@@%tsxd!6k*9v8(gfvke+a2(2zwm-z8qivec#ci*b!")
