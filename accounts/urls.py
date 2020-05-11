e092510d8a19ad9b817e249427c62ad6788b1f5e
Elisa Munoz <elisamunozes@gmail.com>
2020-05-11 18:49:22 +0100

    addese views import

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from .import views
+from .views import signup_view
 
 
 app_name ='accounts'
 
 urlpatterns = [
-    url(r'^signup/$', views.signup_view, name="signup")
+    url(r'^signup/$', signup_view, name="signup")
 ]
