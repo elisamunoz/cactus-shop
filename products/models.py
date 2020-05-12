8027996bc31e91c86afcf34bc707133ee5a19fc2
Elisa Munoz <elisamunozes@gmail.com>
2020-05-12 12:40:37 +0100

    created Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_lengh=100, default='No name')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    image = models.ImageField(upload_to='images')
+
+
+
+#  name = models.CharField(max_length=254, default='')
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=6, decimal_places=2)
+#     image = models.ImageField(upload_to='images')
+
+#     def __str__(self):
+#         return self.name
