cb0e37b3a816c0f767ee2290243946a96abdb3ae
Elisa Munoz <elisamunozes@gmail.com>
2020-05-18 00:07:09 +0100

    added login viwe

--- accounts/views.py
+++ accounts/views.py
@@ -18,3 +18,16 @@ def signup_view(request):
     return render(request, 'signup.html', {'form': form})
 
 
+def login_view(request):
+    """ This view manages the login """
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log in the user
+            user = form.get_user()
+            login(request, user)
+            return redirect("products_list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {'form': form})
+
