f7f30ee7a4f6de49999c3f05c91749288f7a9b61
Elisa Munoz <elisamunozes@gmail.com>
2020-05-17 15:20:55 +0100

    added product details.html, urls and views

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import products_list
+from .views import products_list, product_detail
 
 
 urlpatterns = [
-    url(r'^$', products_list),
+    url(r'^$', products_list, name="products_list"),
+    url(r'^(?P<pk>[\w-]+)/$', product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.http import HttpResponse
 
 
 def products_list(request):
+    """ Creates a view that returns a products lists
+    """
     products = Product.object.all().order_by('?', 'name')
     return render(request, 'products.html', {'products':products})
 
+def product_detail(request, pk):
+    """ Creates a view that returns a single Products based on its ID or returns a 404 error if product doesn't exist
+    """
+    product = get_object_or_404(Product, pk=pk )
+    product.save()
+    return render(request, "productdetail.html", {'product':product})
