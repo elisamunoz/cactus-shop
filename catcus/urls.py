755daf21d6ae2c87eb8e7d75d3b35ac5c1821a02
Elisa Munoz <elisamunozes@gmail.com>
2020-05-17 18:39:52 +0100

    fixed settings

--- catcus/urls.py
+++ catcus/urls.py
@@ -15,4 +15,4 @@ urlpatterns = [
 
 
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(setting.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
