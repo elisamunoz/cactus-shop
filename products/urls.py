6e8a1228ccd93ac9e1026778c16a24e0333e9f2c
Elisa Munoz <elisamunozes@gmail.com>
2020-05-21 00:07:05 +0100

    create product template, url and functions

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import products_list, product_detail
+from .views import products_list, product_detail, product_create
 
 
 urlpatterns = [
     url(r'^$', products_list, name="products_list"),
+    url(r'^create/$', product_create, name="create"),
     url(r'^(?P<pk>[\w-]+)/$', product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import CreateProduct
 
 
 def products_list(request):
@@ -9,9 +10,22 @@ def products_list(request):
     products = Product.object.all().order_by('?', 'name')
     return render(request, 'products.html', {'products':products})
 
+
 def product_detail(request, pk):
     """ Creates a view that returns a single Products based on its ID or returns a 404 error if product doesn't exist
     """
     product = get_object_or_404(Product, pk=pk )
     product.save()
     return render(request, "productdetail.html", {'product':product})
+
+
+@login_required(login_url="accounts/login")
+def product_create(request):
+    if request.method == 'POST':
+        form = CreateProduct(request.POST, request.FILES)
+        if form.is_valid():
+            # save article to db
+            return redirect('products_list')
+    else:
+        form = CreateProduct()
+    return render(request, 'productcreate.html', {'form':form})
