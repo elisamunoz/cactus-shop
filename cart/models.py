7aa88bff2b718c0b40ce2772c7cafa623ea43649
Elisa Munoz <elisamunozes@gmail.com>
2020-06-01 14:03:30 +0100

    changed cart views and urls

--- cart/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from products.models import Product
-
-class Cart(models.Model):
-    products = models.ManyToManyField(Product, null=True, blank=True)
-    total = models.DecimalField(max_digits=6, decimal_places=2, default= 0.0)
-
-    def __unicode__(self):
-        return "cart id: %s" %(self.id)
