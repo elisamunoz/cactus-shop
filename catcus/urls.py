a6054f20dbfac45538d74ce59f831be1c442c3c1
Elisa Munoz <elisamunozes@gmail.com>
2020-06-06 18:58:08 +0100

    created checkout html and linkx

--- catcus/urls.py
+++ catcus/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from .views import about, home
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-# from django.conf.urls.static import static
+from checkout import urls as urls_checkout
 from django.views import static
 from django.conf import settings
 from .settings import MEDIA_ROOT
@@ -15,6 +15,7 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^products/', include('products.urls')),
     url(r'^cart/', include('cart.urls')),
+    url(r'checkout/', include(urls_checkout)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
 
