98d92cea85fa0aedce073086ba57e2a7569391eb
Elisa Munoz <elisamunozes@gmail.com>
2020-06-06 19:00:08 +0100

    created stripe javascript

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,6 +25,5 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = (
             'full_name', 'phone_number', 'country', 'postcode',
-            'town_or_city', 'street_address1', 'street_address2',
-            'county'
+            'city_town', 'address'
         )
