4507da17439be49dbd5420c26b6463def95e9db7
Elisa Munoz <elisamunozes@gmail.com>
2020-07-01 10:01:06 +0100

    added javascript to make nabvar smaller when scrolling down

--- static/js/script.js
+++ static/js/script.js
@@ -1,41 +1,12 @@
-// $(document).ready(function() {
-//     $(window).scroll(fixedNavBar);
-//   });
+$(document).ready(function() {
+    $(window).scroll(fixedNavBar);
+});
 
 
-// //This function makes the navbar fixed after scrolling 200px down
+// //This function toggles a class in the navbar after 200px
 
-// function fixedNavBar() {
-//   if ($(this).scrollTop() > 200) {
-//     $(".navbar").addClass("navbar-small");
-//     $(".navbar-brand").addClass("navbar-brand-small");
-//     $(".catcus-logo").addClass("catcus-logo-small");
-//     $(".navbar-nav").addClass("navbar-nav-small");
-//   } else {
-//     $(".navbar").removeClass("navbar-small");
-//     $(".navbar-brand").removeClass("navbar-brand-small");
-//     $(".catcus-logo").removeClass("catcus-logo-small");
-//     $(".navbar-nav").removeClass("navbar-nav-small");
-//   }
-// }
-
-
-// /* Smaller navbar */
-// .navbar-small {
-//     min-height: 60px;
-//   }
-  
-//   .navbar-brand-small {
-//     font-size: 1.7rem;
-//   }
-  
-//   .catcus-logo-small {
-//     width: 40px;
-//   }
-  
-//   .navbar-nav-small {
-//     font-size: 12px;
-//   }
-  
-//   /* /Smaller navbar */
-  
+function fixedNavBar() {
+    $(".navbar__wrapper").toggleClass(
+        "navbar__wrapper--small",
+        $(this).scrollTop() > 200);
+    }
